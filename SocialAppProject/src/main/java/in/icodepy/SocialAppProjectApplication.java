6168f398a94087de7a455dcffbfb22e5991b7a0b
Amir Hasan <amirhsn896@gmail.com>
2017-01-03 16:24:47 +0530

    First Commit!!

---
+++ SocialAppProject/src/main/java/in/icodepy/SocialAppProjectApplication.java
@@ -0,0 +1,12 @@
+package in.icodepy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialAppProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialAppProjectApplication.class, args);
+	}
+}
---
+++ SocialAppProject/src/test/java/in/icodepy/SocialAppProjectApplicationTests.java
@@ -0,0 +1,16 @@
+package in.icodepy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SocialAppProjectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
